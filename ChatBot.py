43ba40fef1da5cd6f7abeb9e22681d932794b10a
sibims <sibima00@gmail.com>
2024-04-09 11:18:37 +0530

    return the decoded response

--- ChatBot.py
+++ ChatBot.py
@@ -9,3 +9,16 @@ model_inst=GPT2LMHeadModel.from_pretrained_model(model_id)
 
 # Set the model to evaluation mode
 model_id.eval()
+
+def generate_response(prompt,max_length=50):
+
+    #tokenize the prompt
+    input=tokenizer.encode(prompt,return_tensor='pt')
+
+    #Generate response
+    with torch.no_grad():
+        output=model_id.generate(input,max_length=max_length,num_return_sequance=1,pad_token_id=50256)
+
+    #Decoding the ouput
+    response=tokenizer.decode(output[0],skip_special_tokens=True)
+    return response
